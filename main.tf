b79b859513362181cb058b8880e6717aac65e3ed
EdgardoAleChamorro <edgardoale15@gmail.com>
2026-06-20 01:18:22 -0300

    servicios prometheus y grafana,en docker y conectado a terraform

--- main.tf
+++ main.tf
@@ -8,11 +8,16 @@ terraform {
 }
 
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  host = var.docker_host
 }
 
 resource "docker_image" "appimage" {
   name = "pin-proyecto1-app:latest"
+  build {
+    context    = "${path.module}"
+    dockerfile = "Dockerfile"
+    target     = "production"
+  }
 }
 
 resource "docker_container" "app_container" {
@@ -23,5 +28,53 @@ resource "docker_container" "app_container" {
     internal = 3000
     external = var.external_port
   }
+}
+
+# Prometheus
+resource "docker_volume" "prometheus_data" {}
+
+resource "docker_image" "prometheus" {
+  name = "prom/prometheus:latest"
+}
+
+resource "docker_container" "prometheus" {
+  image = docker_image.prometheus.image_id
+  name  = "prometheus"
+
+  ports {
+    internal = 9090
+    external = var.prometheus_port
+  }
+
+  volumes = [
+    "${path.module}/prometheus.yml:/etc/prometheus/prometheus.yml:ro",
+    "${docker_volume.prometheus_data.name}:/prometheus",
+  ]
+
+  command = [
+    "--config.file=/etc/prometheus/prometheus.yml",
+    "--storage.tsdb.path=/prometheus",
+  ]
+}
+
+# Grafana
+resource "docker_volume" "grafana_data" {}
+
+resource "docker_image" "grafana" {
+  name = "grafana/grafana:latest"
+}
+
+resource "docker_container" "grafana" {
+  image = docker_image.grafana.image_id
+  name  = "grafana"
+
+  ports {
+    internal = 3000
+    external = var.grafana_port
+  }
+
+  volumes = [
+    "${docker_volume.grafana_data.name}:/var/lib/grafana",
+  ]
 }
 
--- variables.tf
+++ variables.tf
@@ -7,3 +7,19 @@ variable "external_port" {
   type    = number
   default = 3000
 }
+
+variable "docker_host" {
+  type    = string
+  default = "npipe:////./pipe/docker_engine"
+  description = "Docker host connection string. On Linux use unix:///var/run/docker.sock"
+}
+
+variable "prometheus_port" {
+  type    = number
+  default = 9090
+}
+
+variable "grafana_port" {
+  type    = number
+  default = 3000
+}
